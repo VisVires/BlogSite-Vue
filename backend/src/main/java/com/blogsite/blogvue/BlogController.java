2b7501e53d991af43c223c2453d98eac0b7727c2
VisVires <wgeorge@outlook.com>
2020-06-06 14:13:48 -0700

    dev: updated and added GET endpoints for website

--- backend/src/main/java/com/blogsite/blogvue/BlogController.java
+++ backend/src/main/java/com/blogsite/blogvue/BlogController.java
@@ -2,8 +2,31 @@ package com.blogsite.blogvue;
 
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 public class BlogController {
 	
+	@GetMapping("/jobs")
+	public String getJobs() {
+		return "Jobs";
+	}
+	
+	@GetMapping("/projects")
+	public String getProjects() {
+		return "Projects";
+	}
+	
+	@GetMapping("/education")
+	public String getEducation() {
+		return "Education";
+	}
+	
+	@GetMapping("/techBlog")
+	public String getTechBlog() {
+		return "Tech Blog";
+	}
+	
+	@GetMapping("/travelBlog")
+	public String getTravelBlog() {
+		return "Travel Blog";
+	}
 }
