7253be5f85b87dc5792a1ca5eb338c9a3407778b
VisVires <wgeorge@outlook.com>
2020-06-10 20:09:50 -0700

    dev: changed footer style

--- blog-site/src/components/Footer.vue
+++ blog-site/src/components/Footer.vue
@@ -6,23 +6,21 @@
         <p class="footer">
           <a href="https://www.linkedin.com/in/georgw1/">
           <font-awesome-icon :icon="['fab', 'linkedin']">
-          </font-awesome-icon> LinkedIn </a>
-        </p>
-        <p class="footer">
+          </font-awesome-icon> LinkedIn </a><br>
           <a href="mailto:wgeorge@outlook.com">
           <font-awesome-icon :icon="['far', 'envelope']">
-          </font-awesome-icon> Email </a>
-        </p>
-        <p class="footer">
+          </font-awesome-icon> Email </a><br>
           <a href="https://github.com/VisVires">
           <font-awesome-icon :icon="['fab', 'github']">
-          </font-awesome-icon> GitHub </a>
-        </p>
-        <p class="footer">
+          </font-awesome-icon> GitHub </a><br>
           <a href="https://instagram.com/visviresvita">
           <font-awesome-icon :icon="['fab', 'instagram']">
           </font-awesome-icon> Instagram </a>
         </p>
+        <div style="text-align: right">
+          <h5>Created by William George</h5>
+          <h6>Powered by Vue.js</h6>
+        </div>
       </div>
     </footer>
   </div>
