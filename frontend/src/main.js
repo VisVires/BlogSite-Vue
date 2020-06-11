980b654450e30cebc36c2b82690799fab544a946
VisVires <wgeorge@outlook.com>
2020-06-11 11:58:25 -0700

    dev: made small changes and added appropriate packages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/assets/css/custom.css'
+
+library.add(faGithub, faInstagram, faLinkedin, faEnvelope)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
+
+Vue.mixin({
+  methods: {
+    convertDate: function (item) {
+      if (item === null) {
+        return 'Current'
+      }
+      var date = new Date(item)
+      var ms = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      return ms[date.getMonth()] + ' ' + date.getFullYear()
+    }
+  }
+})
 
 new Vue({
   router,
