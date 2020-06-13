33f22a9aed90db1fc26d9d1c54d5b0ca5157a399
VisVires <wgeorge@outlook.com>
2020-06-13 14:34:39 -0700

    dev: added admin info to nav when active

--- blog-site/src/components/NavBar.vue
+++ blog-site/src/components/NavBar.vue
@@ -8,10 +8,33 @@
         <b-nav-item to="/projects">PROJECTS</b-nav-item>
         <b-nav-item to="/resume">RESUME</b-nav-item>
         <b-nav-item to="/photography">PHOTOGRAPHY</b-nav-item>
+        <b-nav-item v-if="showAdmin" to="/admin">ADMIN</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
 
 <script>
+
+export default {
+  name: "NavBar",
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+    showAdmin() {
+      if (this.currentUser && this.currentUser.roles) {
+        return this.currentUser.roles.includes('ROLE_ADMIN');
+      }
+      return false;
+    }
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
+    }
+  }
+}
+
 </script>
--- blog-site/src/components/SideBar.vue
+++ blog-site/src/components/SideBar.vue
@@ -6,9 +6,25 @@
       <b-list-group-item to="/projects" exact exact-active-class="active" class="sidebar-list-group">PROJECTS</b-list-group-item>
       <b-list-group-item to="/resume" exact exact-active-class="active" class="sidebar-list-group">RESUME</b-list-group-item>
       <b-list-group-item to="/photography" exact exact-active-class="active" class="sidebar-list-group">PHOTOGRAPHY</b-list-group-item>
+      <b-list-group-item v-if="showAdmin" to="/admin" exact exact-active-class="active" class="sidebar-list-group">ADMIN</b-list-group-item>
     </b-list-group>
   </div>
 </template>
 
 <script>
+
+export default {
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+    showAdmin() {
+      if (this.currentUser && this.currentUser.roles) {
+        return this.currentUser.roles.includes('ROLE_ADMIN');
+      }
+      return false;
+    }
+  }
+}
+
 </script>
