9ad1258b0d5d426640553021b83320fca84bab13
VisVires <wgeorge@outlook.com>
2020-06-29 12:56:03 -0700

    master: more mobile styles

--- blog-site/src/views/Tech.vue
+++ blog-site/src/views/Tech.vue
@@ -6,7 +6,7 @@
       <p class="lead blog-description"></p>
     </div>
     <div class="container-fluid">
-      <div class="row">
+      <div class="row" v-if="!isMobile">
         <div class="col-sm-8">
           <Quote></Quote>
           <hr>
@@ -23,13 +23,30 @@
           </div>
         </div>
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-          <div v-if="!isMobile" class="sidebar-module sidebar-module-inset">
+          <div class="sidebar-module sidebar-module-inset">
             <h4>About</h4>
               <p>The purpose of this blog is to both give the reader a window into <em>what I'm learning right now</em> and specific topics that interest me in the broad world of Computer Science</p>
           </div>
           <div class="sidebar-module">
           </div>
           <div class="sidebar-module">
+          </div>
+        </div>
+      </div>
+      <div class="row" v-else>
+        <div class="col-sm-12">
+          <Quote></Quote>
+          <hr>
+          <div id="techblog-post" v-if="posts.length">
+            <TechBlog :currPost="currPost"></TechBlog>
+            <b-button-group>
+              <b-button size="lg" variant="outline-secondary" type="button" @click="prevPage" :disabled="currentPage==0">Prev Post</b-button>
+              <b-button size="lg" variant="outline-secondary" type="button" @click="nextPage" :disabled="currentPage >= pages-1">Next Post</b-button>
+            </b-button-group>
+            <hr>
+          </div>
+          <div v-else>
+            <h5>Sorry! There seems to be an issue, hit the email button below and let me know!</h5>
           </div>
         </div>
       </div>
